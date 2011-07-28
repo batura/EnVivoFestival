b33387f389f552d45a54fd8302451bcb5e146a41
Eneko Nieto <eneko@baturamobile.com>
2011-07-28 22:04:26 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+ 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width,user-scalable=0,minimum-scale=1,maximum-scale=1" />
+
+	<script type="text/javascript">
+
+
+	</script>
+</head>
+<body>
+<?php
+// require_once 'lib/mdetect.php';
+// 
+// $uagent = new uagent_info();
+// 
+// if (isset($_REQUEST['qr'])) {
+//   if(!$uagent->DetectJQueryMobile()){
+// 	header('Location: http://batuin.com/wap/361725/5');
+//   } else {
+// 	header('Location: logo_qr.html');
+//   }
+// }
+// else {
+//   if(!$uagent->DetectJQueryMobile()){
+// 	header('Location: http://batuin.com/wap/361725');
+//   } else {
+	header('Location: logo.html');
+//   }
+// }
+?>
+</body></html>
