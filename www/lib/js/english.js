f5f68cd83bee5c2136b8402252d6c056505cec1e
Eneko Nieto <eneko@baturamobile.com>
2011-07-29 12:10:31 +0200

    Files for phonegap build

---
+++ www/lib/js/english.js
@@ -0,0 +1,487 @@
+ 
+  var lang_en=[];
+  //lang_en['autobus']=[];
+  //  lang_en['']='';
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //Como llegar
+  lang_en['llegar']= '¿Cómo llegar?'; 
+  
+  //PAGINA AUTOBUS
+  lang_en['autobus']='Autobuses gratuitos';
+  lang_en['autobus1']='';
+  
+  //PAGINA ACCESO PEATONAL
+  lang_en['peatonal']='Acceso peatonal';
+  lang_en['peatonal1']='';
+  
+  
+  lang_en['taxis']='Taxis';
+  lang_en['taxis1']='';
+  
+  
+  //PAGINA PARKING
+  lang_en['parking']='Parking';
+  lang_en['parking1']='';
+  
+  //PAGINA METRO
+  lang_en['metro']='Metro';
+  
+  
+  lang_en['masmedios']='Otros medios de transporte en Bilbao';
+  lang_en['masmedios1']='';
+  
+  
+  //
+  //por confirmar
+  lang_en['minusvalidos']='Minusválidos';
+  lang_en['minusvalidos1']='';
+  
+  lang_en['bonosp']='Bonos y pulseras';
+  lang_en['bonosp1']=''; 
+  
+  lang_en['mercadillo']='Mercadillo';
+  lang_en['mercadillo1']='';
+
+  lang_en['wcs']='WC'+"'"+'s';  
+  lang_en['wcs1']='';  
+  
+  lang_en['restricciones']='Restricciones de edad';
+  lang_en['restricciones1']='';
+  
+  lang_en['apertura']='¿A qué hora se abren las puertas?';
+  lang_en['apertura1']='';
+  
+  lang_en['peligrosos']='Objetos peligrosos';
+  lang_en['peligrosos1']='';  
+  
+  lang_en['acampada']='Zona de acampada';
+  lang_en['acampada1']='';
+  
+  lang_en['acampadab']='¿Cuándo se abre la zona de acampada?';
+  lang_en['acampadab1']='';
+  
+  lang_en['medicos']='Servicios medicos';
+  lang_en['medicos1']='';
+  
+  lang_en['comida']='Comida y bebida';  
+  lang_en['comida1']='';
+  
+  lang_en['caravanas']='Autocaravanas';
+  lang_en['caravanas1']='';
+  
+  //ACERCA DE
+  lang_en['acercade']='Acerca de...';
+  
+  lang_en['consejos']='Consejos';
+  lang_en['consejos1']='';  
+  
+  lang_en['pulseras']='Entradas con pulseras';
+  lang_en['pulseras1']='';
+  
+  lang_en['conservacion']='Conservación de la zona';
+  lang_en['conservacion1']='';
+  
+  
+  
+  
+//Dias
+  lang_en['7julio']= '7 Julio';  
+  lang_en['8julio']= '8 Julio';  
+  lang_en['9julio']= '9 Julio';    
+//Escenarios
+  lang_en['escenario1']= 'Escenario 1';  
+  lang_en['escenario2']= 'Escenario 2'; 
+  
+  //ARTISTAS dia-escenario-num  
+  
+  
+  //dia 7 escenario 1
+  lang_en['artista711']= '';
+  lang_en['artista711hora']= '';
+  
+  lang_en['artista712']= '';
+  lang_en['artista712hora']= '';
+  
+  lang_en['artista713']= '';  
+  lang_en['artista713hora']= '';  
+  
+  lang_en['artista714']= '';  
+  lang_en['artista714hora']= '';  
+  
+  //dia 7 escenario 2 
+  lang_en['artista721']= '';  
+  lang_en['artista721hora']= '';
+  
+  lang_en['artista722']= '';  
+  lang_en['artista722hora']= '';
+  
+  lang_en['artista723']= '';  
+  lang_en['artista723hora']= ''; 
+  
+  lang_en['artista724']= '';  
+  lang_en['artista724hora']= ''; 
+  
+
+  
+  //dia 8 escenario 1 
+
+  lang_en['artista811']= '';
+  lang_en['artista811hora']= '';  
+
+  lang_en['artista812']= '';
+  lang_en['artista812hora']= '';
+  
+  lang_en['artista813']= '';
+  lang_en['artista813hora']= '';
+  
+
+  
+  lang_en['artista815']= '';
+  lang_en['artista815hora']= '';
+  
+  //dia 8 escenario 2
+  
+  lang_en['artista821']= '';
+  lang_en['artista821hora']= '';
+  lang_en['artista822']= '';
+  lang_en['artista822hora']= '';
+  lang_en['artista823']= '';
+  lang_en['artista823hora']= '';
+  lang_en['artista824']= '';
+  lang_en['artista824hora']= '';
+  
+  //dia 9 escenario 1
+  
+  lang_en['artista911']= '';
+  lang_en['artista911hora']= '';
+  lang_en['artista912']= '';
+  lang_en['artista912hora']= '';
+  lang_en['artista913']= '';
+  lang_en['artista913hora']= '';
+  lang_en['artista914']= '';
+  lang_en['artista914hora']= '';
+  lang_en['artista915']= '';
+  lang_en['artista915hora']= '';
+  lang_en['artista814']= '';
+  lang_en['artista814hora']= '';
+  
+  //dia 9 escenario 2
+  lang_en['artista921']= '';
+  lang_en['artista921hora']= '';
+  
+  lang_en['artista922']= '';
+  lang_en['artista922hora']= '';
+    
+  lang_en['artista923']= '';
+  lang_en['artista923hora']= '';
+    
+  lang_en['artista924']= ' ';
+  lang_en['artista924hora']= '';
+    
+
+    
+//dia 7
+  //escen1
+  lang_en['artista711texto']='';
+  lang_en['artista712texto']='';
+  lang_en['artista713texto']='';
+  lang_en['artista714texto']='';
+
+  //escen2
+  lang_en['artista721texto']='';
+  lang_en['artista722texto']='';
+  lang_en['artista723texto']='';
+  lang_en['artista724texto']='';
+  
+//dia 8   
+  //escen1
+  lang_en['artista811texto']='';  
+  lang_en['artista812texto']='';
+  lang_en['artista813texto']='';
+
+  lang_en['artista815texto']='';
+  //escen2
+  lang_en['artista821texto']='';   
+  lang_en['artista822texto']='';     
+  lang_en['artista823texto']='';   
+  lang_en['artista824texto']='';   
+  
+//dia 9   
+  //escen1
+  lang_en['artista911texto']='';
+  lang_en['artista912texto']='';
+  lang_en['artista913texto']='';
+  lang_en['artista914texto']='';
+  lang_en['artista915texto']='';
+  
+  lang_en['artista814texto']='';
+  
+  //escen2
+  lang_en['artista921texto']='';   
+  lang_en['artista922texto']='';   
+  lang_en['artista923texto']='';   
+  lang_en['artista924texto']='';   
+  
+//Links artistas  
+//dia 7
+  //escen1
+  lang_en['artista711links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista712links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista713links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista714links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista721links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista722links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista723links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista724links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista811links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista812links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista813links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista814links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista815links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista821links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista822links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista823links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista824links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+
+  lang_en['artista911links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista912links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista913links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista914links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista915links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista921links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista922links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista923links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>';
+  
+  lang_en['artista924links']='<a href="">Página web</a><br/><br/>'+
+			     '<a href="">Video 1</a><br/><br/>'+
+			     '<a href="">Video 2</a><br/><br/>'; 
+  
+  //apartado tickets
+  
+  
+  lang_en['tipos']='Tickets';
+  lang_en['tipos1']='';
+  
+  lang_en['pventa']='Puntos de venta';
+  lang_en['pventa1']='Otros puntos de venta';
+  lang_en['pbbk1']='';
+  lang_en['pbbk2']='';	   
+  lang_en['pbbk3']='';
+  
+  lang_en['tm1']='';
+  lang_en['opv']='';
+  
+  
+  
+  //restaurantes
+  lang_en['restaurantes']='Restaurantes';  
+  //por menos de 15
+  lang_en['restaurantes1']='-15€';    
+  lang_en['restaurantes2']='15-35€';    
+  lang_en['restaurantes3']='35-50€';  
+  lang_en['restaurantes4']='+50€';
+  lang_en['restlist1']='';
+  
+  lang_en['restlist2']='';
+
+  lang_en['restlist3']='';
+  
+  lang_en['restlist4']='';
+
+  
+  //HOTELES
+  lang_en['hoteles']='Hoteles';
+  //5estrellas
+  lang_en['hoteles5']='*****';
+  lang_en['hoteles4']='****';
+  lang_en['hoteles3']='***';
+  lang_en['hoteles2']='**';
+      
+  
+  
+  lang_en['hotellist5']='';
+
+//
+
+  lang_en['hotellist4']='';
+  
+  lang_en['hotellist3']='';
+
+
+
+  lang_en['hotellist2']='';
+
+
+
+
+
+
+ 
+//
+  //Playas
+  lang_en['playas']='Playas';
+  lang_en['playa1']='Playa de Arrietara en Sopelana'
+  lang_en['playa1t']='';
+  lang_en['playa2']='Barinatxe (La Salvaje) en Larrabasterra';
+  lang_en['playa2t']='';
+  lang_en['playa3']= 'Gorrondatxe (Azkorri - Getxo)'; 
+  lang_en['playa3t']= '';
+  lang_en['playa4']='Arrigunaga  en Getxo';
+  lang_en['playa4t']= '';
+
+  
+  //OCIO
+  lang_en['ocio']='Lugares de interes';
+  
+  lang_en['ocio1']='Alhóndiga Bilbao';
+  lang_en['ocio1t']='';
+
+  lang_en['ocio2']='Museo Bellas Artes de Bilbao';
+  lang_en['ocio2t']='';
+  
+ 
+  lang_en['ocio3']='Museo Guggenheim Bilbao';
+  lang_en['ocio3t']='';
+  
+  lang_en['ocio4']='Casco Viejo de Bilbao';
+  lang_en['ocio4t']='';
+  
+ 
+//FAVORITOS  
+  lang_en['favoritos']='Favoritos';   
+  lang_en['nofav']='Eliminado de favoritos';
+  lang_en['nofav1']='Esta actuación ha sido eliminada de tu lista de favoritos';  
+  lang_en['nofav2']='Volver'; 
+  lang_en['fav']='Añadido a favoritos';
+  lang_en['fav1']='Esta actuación ha sido añadida a tu lista de favoritos';
+  lang_en['fav2']='Ir a favoritos';
+  lang_en['fav3']='Volver';
+
+  
+  
+  lang_en['iconfav']=  '<img src="lib/images/favoritos_nosel30.png"/> ';
+  lang_en['iconnofav']=  '<img src="lib/images/favoritos_nosel30.png"/> ';
+  
+  
+  
+  
+  
+  
+  /*
+  
+  lang_en['']='';
+  lang_en['']='';
+  lang_en['']='';
+  lang_en['']='';
+  lang_en['']='';
+  lang_en['']='';
+  lang_en['']='';
+  lang_en['']='';
+  lang_en['']='';
+  lang_en['']='';
+  <a href="tel:0034aaaaaa">Teléfono:aaaaaaaaa</a>
+  */
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
